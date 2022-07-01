e3421383acbea4b4b7a5bc72da7460784f7ff8e2
Ash Bhimasani <ash@bhimasani.com>
2022-07-01 12:27:26 -0400

    more mobile

--- app/components/divs/twoColumn.tsx
+++ app/components/divs/twoColumn.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
+import { BREAKPTS } from '../../constants/styles';
 import { FlexCenterColumn } from '../flexs';
 
 export const TwoColumnContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   border: 1px solid #e0e0e0;
   grid-gap: 1px;
   background-color: rgba(0, 0, 0, 0.1);
@@ -17,5 +18,8 @@ export const TwoColumnContentContainer = styled(FlexCenterColumn)`
   padding: 100px;
   > div + div {
     margin-top: 100px;
+  }
+  @media (max-width: ${BREAKPTS.LG}px) {
+    padding: 50px;
   }
 `;
