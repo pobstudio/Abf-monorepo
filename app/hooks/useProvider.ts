cd9c0be9f7e3f5ee669311df7eb287969ef46ce5
Ash Bhimasani <ash@bhimasani.com>
2022-07-12 20:58:39 -0400

    TOGGLE fallback to Alchemy if no wallet

--- app/hooks/useProvider.ts
+++ app/hooks/useProvider.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { PROVIDER } from '../clients/provider';
 import { usePriorityProvider } from '../connectors/priority';
 
-export const useProvider = (shouldUseFallback: boolean = false) => {
+export const useProvider = (shouldUseFallback: boolean = true) => {
   const library = usePriorityProvider();
   return useMemo(
     () => (shouldUseFallback && !library ? PROVIDER : library),
