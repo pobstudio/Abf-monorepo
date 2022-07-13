a8411ed2b1cc9efb2eddaabc562d8033fd859ad7
Ash Bhimasani <ash@bhimasani.com>
2022-07-12 23:13:15 -0400

    ADD /collection/[address] boilerplate

---
+++ app/components/layouts/collection.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { DetailRowsContainer } from '../details/rows';
+import {
+  OneColumnContainer,
+  OneColumnContentContainer,
+} from '../divs/oneColumn';
+import { B, H1, P } from '../texts';
+
+export const Collection: React.FC<{address: string}> = ({address}) => {
+  return (
+    <>
+      <OneColumnContainer>
+        <OneColumnContentContainer>
+          <DetailRowsContainer>
+            <div>
+              <H1 style={{ opacity: 0.2 }}>COMING SOON</H1>
+            </div>
+            <P>
+              Pardon the dust, the ABF Corp is currently building a
+              minting/collecting experience for ABF NFTs. Stay tuned for when
+              the <B>ABF [BETA]</B> drops.
+            </P>
+            <P style={{ opacity: 0.2 }}>
+              {
+                '++[---------->+<]>.+[--->++++<]>+.+++++.++++++.[++>---<]>--.------------.---[->++++<]>-.-----------.-------.--[--->+<]>---.-------------.-[->+++<]>.------------.+[->+++<]>++.[--->+<]>+.--------.----.+++.+++.-------------.--[--->+<]>-.[->++<]>+.+.++++.'
+              }
+            </P>
+          </DetailRowsContainer>
+        </OneColumnContentContainer>
+      </OneColumnContainer>
+    </>
+  );
+};
---
+++ app/pages/collection/[address].tsx
@@ -0,0 +1,32 @@
+import { ADDRESS_REGEX } from '@abf-monorepo/types';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import React, { useMemo } from 'react';
+import { PageDiv } from '../../components/divs';
+import { Collection } from '../../components/layouts/collection';
+import { Footer } from '../../components/navs/footer';
+import { Header } from '../../components/navs/header';
+
+const CollectionIndexPage: NextPage = () => {
+  const router = useRouter();
+  const address = useMemo(() => {
+    if (!router.query.address) {
+      return undefined;
+    }
+    if (
+      typeof router.query.address !== 'string' ||
+      !ADDRESS_REGEX.test(router.query.address)
+    ) {
+      return undefined;
+    }
+    return router.query.address as string;
+  }, [router]);
+  return (
+    <PageDiv>
+      <Header />
+      <Collection address={address} />
+      <Footer />
+    </PageDiv>
+  );
+};
+export default React.memo(CollectionIndexPage);
