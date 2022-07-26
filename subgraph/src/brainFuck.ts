b5bed71ce32764cf001ac1bca6fe226cc4cd0440
dave4506 <me@bydavidsun.com>
2022-07-26 14:29:00 -0500

    ADD transfer events to NFT collection

--- subgraph/src/brainFuck.ts
+++ subgraph/src/brainFuck.ts
@@ -1,8 +1,9 @@
 import { CreatedBrainFuckNFT } from '../generated/BrainFuckFactory/BrainFuckFactory';
-import { Collection } from '../generated/schema';
+import { Collection, Token, TokenTransfer } from '../generated/schema';
 import { BrainFuck as BrainFuckTemplate } from '../generated/templates';
 import {
   BrainFuck as BrainFuckContract,
+  Transfer,
   OwnershipTransferred,
 } from '../generated/templates/BrainFuck/BrainFuck';
 
@@ -38,4 +39,29 @@ export function handleBrainFuckOwnershipTransferred(
     nft.owner = event.params.newOwner;
     nft.save();
   }
+}
+
+export function handleBrainFuckTransfer(event: Transfer): void {
+  let tokenId = event.params.tokenId.toString();
+  let from = event.params.from;
+  let to = event.params.to;
+  let token = Token.load(tokenId);
+  if (token == null) {
+    token = new Token(tokenId);
+    token.collection = event.address.toHexString();
+  }
+  token.owner = to;
+  token.save();
+
+  let transferId = tokenId + '/' + event.block.number.toString();
+  let transfer = TokenTransfer.load(transferId);
+  if (transfer == null) {
+    transfer = new TokenTransfer(transferId);
+    transfer.token = tokenId;
+  } 
+  transfer.to = to;
+  transfer.from = from;
+  transfer.timestamp = event.block.timestamp;
+  transfer.blocknumber = event.block.number;
+  transfer.save();
 }
