2279478d865955bfb942f211824276d65815d6df
dave4506 <me@bydavidsun.com>
2022-08-12 11:36:34 -0500

    ADD better types and sol to export

--- protocol/deployments/types.ts
+++ protocol/deployments/types.ts
@@ -6,8 +6,28 @@ export interface Deployment {
   registries: {
     renderer: string;
   };
-  renderers: { [name: string]: string }; // deployed renderers from this repo
-  utilityRenderers: { [name: string]: string }; // deployed renderers from this repo
+  renderers: {
+    identity: string;
+    dotMatrix: string;
+    path: string;
+    pixelGrid8: string;
+    pixelGrid16: string;
+    pixelGrid24: string;
+    hslPixelGrid8: string;
+    hslPixelGrid16: string;
+    hslPixelGrid24: string;
+    monoPixelGrid8: string;
+    monoPixelGrid16: string;
+    monoPixelGrid24: string;
+    gifImage: string;
+  };
+  utilityRenderers: {
+    configuredGifImage: string;
+    background: string;
+    compactDataMiddleware: string;
+    alphaFilter: string;
+    compositeLayer: string;
+  };
   libraries: {
     vm: string;
     uriConstructor: string;
